eb04990e5d57b5307ee92ccb4e11d2e370ae6ded
Natalie McCroy <nataliemccroy@gmail.com>
2020-05-16 14:39:22 -0500

    sprint mvp completed

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,6 +3,23 @@
 import sys
 
 # op codes
+LDI = 0b10000010 
+HLT = 0b00000001
+PRN = 0b01000111
+MUL = 0b10100010
+POP = 0b01000110
+PUSH = 0b01000101
+CALL = 0b01010000
+RET = 0b00010001
+CMP = 0b10100111
+JMP = 0b01010100
+JNE = 0b01010110
+JEQ = 0b01010101
+
+# flags
+EF = 0b00000001
+LF = 0b00000100
+GF = 0b00000010
 
 class CPU:
     """Main CPU class."""
@@ -12,19 +29,22 @@ class CPU:
         self.ram = [0] * 256
         self.pc = 0
         self.reg = [0] * 8 
-        self.fl = [0] * 8 
+        self.fl = 0 
         self.pointer = 7
         self.reg[self.pointer] = len(self.ram) - 1
         self.branchtable = {
-            0b10000010: self.opcode_LDI,
-            0b00000001: self.opcode_HLT,
-            0b01000111: self.opcode_PRN,
-            0b10100010: self.opcode_MUL,
-            0b01000110: self.opcode_POP,
-            0b01000101: self.opcode_PUSH,
-            0b01010000: self.opcode_CALL,
-            0b00010001: self.opcode_RET,
-            0b10100111: self.opcode_CMP
+            LDI: self.opcode_LDI,
+            HLT: self.opcode_HLT,
+            PRN: self.opcode_PRN,
+            MUL: self.opcode_MUL,
+            POP: self.opcode_POP,
+            PUSH: self.opcode_PUSH,
+            CALL: self.opcode_CALL,
+            RET: self.opcode_RET,
+            CMP: self.opcode_CMP,
+            JMP: self.opcode_JMP,
+            JNE: self.opcode_JNE,
+            JEQ: self.opcode_JEQ
         }
 
     def load(self):
@@ -52,22 +72,19 @@ class CPU:
         """ALU operations."""
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        elif op == self.branchtable[0b10100010]:
+        elif op == MUL:
             self.reg[reg_a] *= self.reg[reg_b]
         #elif op == "SUB": etc
-        elif op == self.branchtable[0b10100111]:
-            if self.reg[reg_a] == self.reg[reg_b]:
-                # set the E flag to 1
-                self.fl = 0b00000001
-            elif self.reg[reg_a] < self.reg[reg_b]:
+        elif op == CMP:
+            if self.reg[reg_a] < self.reg[reg_b]:
                 # set the L flag to 1
-                self.fl = 0b00000100
-            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.fl = LF
+            elif self.reg[reg_a] < self.reg[reg_b]:
                 # set the G flag to 1
-                self.fl = 0b00000010
+                self.fl = GF
             else:
-                # set the flag to 0
-                self.fl = 0b00000000
+                # set the E flag to 1
+                self.fl = EF
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -155,11 +172,12 @@ class CPU:
         # store the next line to execute on the stack
         # return this line after the subroutine 
         self.reg[self.pointer] -= 1
-        self.ram[self.reg[self.pointer]] = self.pc + 2
+        self.ram_write(self.pc + 2, self.reg[self.pointer])
+        # self.ram[self.reg[self.pointer]] = self.pc + 2
         # read which register stores the next line passed w/call 
-        register = self.ram[self.pc + 1]
+        # register = self.ram[self.pc + 1]
         # set the PC to the value in that register
-        self.pc = self.reg[register]
+        self.pc = self.reg[a]
 
     def opcode_RET(self, a, b):
         # pop the current value off stack
@@ -170,7 +188,24 @@ class CPU:
         self.pc = return_address
 
     def opcode_CMP(self, a, b):
-        ir = self.ram[self.pc]
-        x = self.branchtable[ir]
+        x = CMP
         self.alu(x, a, b)
         self.pc += 3
+
+    def opcode_JMP(self, a, b):
+        # set the pc to the address stored in the given register
+        self.pc = self.reg[a]
+        
+    def opcode_JEQ(self, a, b):
+        # if equal flag is set (true), jump to address in given register
+        if self.fl == EF:
+            self.pc = self.reg[a]
+        else:
+            self.pc += 2
+
+    def opcode_JNE(self, a, b):
+        # if equal flag is clear (false), jump to address stored in given register
+        if not self.fl == EF:
+            self.pc = self.reg[a]
+        else:
+            self.pc += 2
